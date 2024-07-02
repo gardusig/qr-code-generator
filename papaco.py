edb022768446c16769b8f92d4f5013219c77cc60
Gustavo Gardusi <gardusig@Gustavos-Laptop.local>
2024-07-02 08:30:11 -0300

    .

--- papaco.py
+++ papaco.py
@@ -1,10 +1,8 @@
 import openpyxl
 import qrcode
-import uuid
 import tempfile
-from reportlab.lib.pagesizes import A4, landscape
+from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
-from PIL import Image, ImageDraw, ImageFont
 
 
 def create_qr_code(data):
@@ -21,55 +19,87 @@ def create_qr_code(data):
     return img
 
 
-def generate_pdf(url: str, password: str):
-    random_id = uuid.uuid4()
+def draw_border_and_corners(c, width, height):
+    c.setStrokeColorRGB(0, 0, 0)
+    c.setLineWidth(3)
+    c.rect(10, 10, width - 20, height - 20)
 
+    c.setLineWidth(3)
+    corner_size = 30
+    c.line(10, height - 10, 10 + corner_size, height - 10)
+    c.line(10, height - 10, 10, height - 10 - corner_size)
+
+    c.line(width - 10, height - 10, width - 10 - corner_size, height - 10)
+    c.line(width - 10, height - 10, width - 10, height - 10 - corner_size)
+
+    c.line(10, 10, 10 + corner_size, 10)
+    c.line(10, 10, 10, 10 + corner_size)
+
+    c.line(width - 10, 10, width - 10 - corner_size, 10)
+    c.line(width - 10, 10, width - 10, 10 + corner_size)
+
+
+def draw_qr_code_and_text(c, qr_img, x, y, max_qr_width, max_qr_height, text, vertical_align='top'):
+    text_height = 30
+    font_size = 12
+    with tempfile.NamedTemporaryFile(suffix=".png") as qr_file:
+        qr_img.save(qr_file.name)
+        qr_text_width = c.stringWidth(text, "Helvetica", font_size)
+
+        # Calculate text position to center it
+        text_x = x + (max_qr_width - qr_text_width) / 2
+
+        if vertical_align == 'top':
+            text_y = y + max_qr_height + 10
+            qr_y = y
+        else:
+            text_y = y - text_height - 10
+            qr_y = y - max_qr_height + 90  # Adjusted value for proper centering
+
+        # Draw text and QR code
+        c.setFont("Helvetica", font_size)
+        c.drawString(text_x, text_y, text)
+        c.drawImage(qr_file.name, x, qr_y,
+                    width=max_qr_width, height=max_qr_height)
+
+
+def add_page_to_pdf(c, url, password):
     url_qr_img = create_qr_code(url)
     password_qr_img = create_qr_code(password)
 
-    pdf_filename = f"/Users/gardusig/Desktop/qrcode/{random_id}.pdf"
-    c = canvas.Canvas(pdf_filename, pagesize=landscape(A4))
-    width, height = landscape(A4)
-    max_qr_width = max_qr_height = (height - 100) / 2  # Leave some margins
-    text_height = 30
-    total_height = max_qr_height + text_height + 20  # 20 for padding
+    width, height = A4
+    max_qr_width = max_qr_height = (height - 200) / 4  # Leave some margins
 
-    with tempfile.NamedTemporaryFile(suffix=".png") as url_qr_file, tempfile.NamedTemporaryFile(suffix=".png") as password_qr_file:
-        url_qr_img.save(url_qr_file.name)
-        password_qr_img.save(password_qr_file.name)
+    draw_border_and_corners(c, width, height)
 
-        # Calculate positions
-        section_width = width / 2
-        url_x = section_width / 2 - max_qr_width / 2
-        password_x = section_width + section_width / 2 - max_qr_width / 2
-        qr_y = height / 2 - max_qr_height / 2
+    # Calculate positions
+    section_height = height / 2
+    qr_x = width / 2 - max_qr_width / 2
+    url_y = section_height + (section_height / 2) - (max_qr_height / 2)
+    # Adjusted this value for proper centering
+    password_y = section_height / 2 + \
+        (section_height / 2) - max_qr_height - 139
 
-        # Measure text width to center the titles
-        url_text = "URL"
-        password_text = "Password"
-        url_text_width = c.stringWidth(url_text, "Helvetica", 12)
-        password_text_width = c.stringWidth(password_text, "Helvetica", 12)
+    draw_qr_code_and_text(c, url_qr_img, qr_x, url_y, max_qr_width,
+                          max_qr_height, "URL", vertical_align='top')
+    draw_qr_code_and_text(c, password_qr_img, qr_x, password_y,
+                          max_qr_width, max_qr_height, "Senha", vertical_align='top')
 
-        # Draw URL text and QR code
-        c.drawString(url_x + max_qr_width / 2 - url_text_width /
-                     2, qr_y + max_qr_height + text_height, url_text)
-        c.drawImage(url_qr_file.name, url_x, qr_y,
-                    width=max_qr_width, height=max_qr_height)
+    # Draw the middle line
+    c.setLineWidth(3)
+    c.line(10, height / 2, width - 10, height / 2)
 
-        # Draw Password text and QR code
-        c.drawString(password_x + max_qr_width / 2 - password_text_width /
-                     2, qr_y + max_qr_height + text_height, password_text)
-        c.drawImage(password_qr_file.name, password_x, qr_y,
-                    width=max_qr_width, height=max_qr_height)
-
-    c.save()
-    print(f"PDF saved as {pdf_filename}")
+    c.showPage()
 
 
 def main():
+    pdf_filename = "/Users/gardusig/Desktop/qrcode/combined.pdf"
+    c = canvas.Canvas(pdf_filename, pagesize=A4)
     workbook = openpyxl.load_workbook('input.xlsx')
     for row in workbook.active.iter_rows(min_row=2, values_only=True):
-        generate_pdf(row[4], row[3])
+        add_page_to_pdf(c, row[4], row[3])
+    c.save()
+    print(f"PDF saved as {pdf_filename}")
 
 
 if __name__ == "__main__":
